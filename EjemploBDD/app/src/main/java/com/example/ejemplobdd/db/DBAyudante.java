fa09f94302a0592b77a470f0e1e2c6a00f5355c8
mateomate1 <mateoayarrabarbero@gmail.com>
2025-02-12 13:48:45 +0100

    12/2/2025

---
+++ EjemploBDD/app/src/main/java/com/example/ejemplobdd/db/DBAyudante.java
@@ -0,0 +1,33 @@
+package com.example.ejemplobdd.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBAyudante extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NOMBRE = "agenda.db";
+    private static final String DATABASE_TABLE = "t_contactos";
+
+    public DBAyudante(@Nullable Context context) {
+        super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+DATABASE_TABLE+"(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nombre TEXT NOT NULL," +
+                "telefono TEXT NOT NULL," +
+                "correo_electronico TEXT)");
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
